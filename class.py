441635f74eca4bae30005d149e3e8587d282a8ce
SYThtou <fbtpduf@gmail.com>
2019-01-21 16:21:00 +0900

    person

---
+++ class.py
@@ -0,0 +1,16 @@
+class Person:
+    def setName(self,name):
+        self.name = name
+    def getNames(self):
+        return self.name
+    def greet(self):
+        print("Hello, I am %s." % self.name)
+
+person1 = Person()
+person2 = Person()
+
+person1.setName('Se Yeol Rhyou')
+person2.setName('Yoon Sung Hyeon')
+
+person1.greet()
+person2.greet()
