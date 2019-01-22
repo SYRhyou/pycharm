09a8c8b5b7d722e4d74f105aba8d7d3ea126414a
SYThtou <fbtpduf@gmail.com>
2019-01-22 16:40:29 +0900

    test2

--- tf.py
+++ tf.py
@@ -13,6 +13,6 @@ mul = a * b
 g = (a * b) + a + b
 sess = tf.Session()
 
-print("Add : %i " %sess.run(add, feed_dict={a:2, b:3}))
-print("Mul : %i " %sess.run(mul, feed_dict={a:2, b:3}))
-print("Graph : %i " %sess.run(g, feed_dict={a:2, b:3}))
+print("Add : %i " %sess.run(add, feed_dict={a:5, b:3}))
+print("Mul : %i " %sess.run(mul, feed_dict={a:5, b:3}))
+print("Graph : %i " %sess.run(g, feed_dict={a:5, b:3}))
