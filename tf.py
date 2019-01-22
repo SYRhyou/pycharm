538be2a4ff964fe856096e101bb8bdab6a3c8f51
SYThtou <fbtpduf@gmail.com>
2019-01-22 16:43:52 +0900

    tensorflow

--- tf.py
+++ tf.py
@@ -11,9 +11,9 @@ b = tf.placeholder(tf.int16)
 add = a + b
 mul = a * b
 g = (a * b) + a + b
-
 sess = tf.Session()
 
-print("Add : %i " %sess.run(add, feed_dict={a:5, b:3}))
-print("Mul : %i " %sess.run(mul, feed_dict={a:5, b:3}))
+print("Add : %i " %sess.run(add, feed_dict={a:2, b:3}))
+print("Mul : %i " %sess.run(mul, feed_dict={a:2, b:3}))
+
 print("Graph : %i " %sess.run(g, feed_dict={a:5, b:3}))
